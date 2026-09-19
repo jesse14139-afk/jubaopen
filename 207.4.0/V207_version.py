a793f612f9820c76e01c3b65fe11e1a3e28ddeea
jesse518 <jesse14139@gmail.com>
2026-09-20 06:48:22 +0800

    删除

--- 207.4.0/V207_version.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""聚宝盆版本单一真相源。"""
-PRODUCT_VERSION = "207.4.0-dev"
-SCHEMA_VERSION = 207
-CATALOG_VERSION = "207.4"
-
-# V207.4.0-dev 更新说明（2026-09-20）：统一产品、Schema 与数据字典版本来源。
